e5e2b0749bcad7a8eea5986af11221561d9d9a10
Tk21111 <101468538+Tk21111@users.noreply.github.com>
2025-04-27 11:56:05 +0700

    Add files via upload

--- Storygame.java
+++ Storygame.java
@@ -104,7 +104,8 @@ public class Storygame {
 
 
     public static void main (String[] args) throws InterruptedException {
-        while (true) {
+        boolean lopp = true;
+        while (lopp) {
             int point = 0 ;
             int Tmp = 0;
             int END = 3;
@@ -166,6 +167,7 @@ public class Storygame {
             System.out.println(YELLOW + "Point : " + Integer.toString(point));
             System.out.println(CYAN + "Thank you for playing :3");
             System.out.println(WHITE + "===================");
+            lopp = false;
         }
         
     }
@@ -273,60 +275,60 @@ public class Storygame {
 
     static void Story1() throws InterruptedException{
         System.out.println(WHITE + "In 1943, in the middle of World War II, The Soviet boy name Michael Macros was captured by the Nazi army during a raid on a border village. \nHis family was killed in front of him, leaving Michael feeling hopeless at the loss of everything.");
-        Thread.sleep(7000);
+        Thread.sleep(500);
         System.out.println(WHITE + "\nThe Nazi brainwashed Michael and trained him to be a strong, sharp and human-sense soldier, \nbut the depths of his mind still left a fragment of old memories.");
-        Thread.sleep(7000);
+        Thread.sleep(500);
         System.out.println(WHITE + "\nAfter Michael brainwashing, he was one of the most trusted Nazi spy until he has an important mission by \nhaving to infiltrate the Soviet command centre and steal high-level secret documents.");
-        Thread.sleep(6000);
+        Thread.sleep(500);
         System.out.println(WHITE + "\nAfter Michael successfully infiltrated, he had to go through both walls before entering the secret documents storage room.");
-        Thread.sleep(3500);
+        Thread.sleep(500);
         System.out.println(WHITE + "\nDeciphering the wall 1 by the code from the year the Soviet Union was founded,");
-        Thread.sleep(3500);
+        Thread.sleep(500);
         System.out.println(BLACK + "with the clue that there are two numbers: [1 , ... , 2 , ...] ");
-        Thread.sleep(3500);
+        Thread.sleep(500);
         System.out.println(WHITE + "===================");
     }
 
     static void Story2() throws InterruptedException{
         System.out.println(WHITE + "After deciphering the 1st wall, Michael explored the 2nd wall and found a secret door that had to be coded in three English letters.");
-        Thread.sleep(3500);
+        Thread.sleep(500);
         System.out.print(BLACK + "that had to be coded in three English letters");
-        Thread.sleep(3500);
+        Thread.sleep(500);
         System.out.println(WHITE + " which has a hint as the initials of the Soviet State Security Committee");
-        Thread.sleep(3500);
+        Thread.sleep(500);
         System.out.println(WHITE + "===================");
     }
 
     static void Stroy3() throws InterruptedException{
         
         System.out.println(WHITE + "When he passed by, he found a secret filing room. The detective decided to open the door. But it turned out that the door could not be opened. So he tried to explore around the room. Found many picture frames. But there is one picture that the detective saw and felt majestic. It is a picture of a general with a child. Which he felt that the child looked like him when he was a child. In these pictures, he saw a boy who playing with a father in a ground.");
-        Thread.sleep(7000);
+        Thread.sleep(500);
         System.out.println(WHITE + "\nSo he suspicious of the picture,but the mission must keep going so he had to decode it in that room. To clear doubts about the picture By the code is letters and numbers Which this code comes from the Soviet leader who ascended the throne in 1924, which has 7 characters, consisting of the first part is three English abbreviations and the second part is numbers.");        System.out.println(WHITE + "===================");
-        Thread.sleep(7000);
+        Thread.sleep(500);
     }
 
     static void Story4() throws InterruptedException{
         System.out.println(GREEN + "After successfully stealing documents,");
-        Thread.sleep(3500);
+        Thread.sleep(500);
         System.out.println(RED +"but while he escaped, he was captured by Soviet troops.");
-        Thread.sleep(3500);
+        Thread.sleep(500);
         System.out.println(WHITE + "\nMichael was dragged to the interrogation room. Amidst the tension and death is approaching, he met General Alexei Makrov, a general who was famous for his cruelty.");
-        Thread.sleep(3500);
+        Thread.sleep(500);
         System.out.println(WHITE + "\nAnd in the split second he saw the general’s face, the sealed memories broke. His father’s face, the warm hands that once held him, the laughter of the days when the village was still lively.");
-        Thread.sleep(3500);
+        Thread.sleep(500);
         System.out.println(WHITE + "\nMichael immediately realised that this was his father, a father who was killed in front of his eyes but was still alive and standing in front of him.");
-        Thread.sleep(3500);
+        Thread.sleep(500);
         System.out.println(WHITE + "As General Alexey aims his gun at Michael, Michael shouts at the top of his lungs.");
-        Thread.sleep(3500);
+        Thread.sleep(500);
         System.out.println(WHITE + "===================");
     }
 
 
     static void GoodEndStory() throws InterruptedException{
         System.out.println(WHITE + "That voice was deeply in the heart of the general. Old memories come back. That’s the son who disappeared in the war!");
-        Thread.sleep(3000);
+        Thread.sleep(500);
         System.out.println(BLACK + "His gun fell out of his hand from then.");
-        Thread.sleep(2000);
+        Thread.sleep(500);
         System.out.println(WHITE + "The father and son approached and hugged each other. And he say“Let’s go home... my son”");
 
     }
